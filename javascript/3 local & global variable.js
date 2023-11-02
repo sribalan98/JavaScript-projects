1d656c92b3ed080055e356033380a30fd0c8d26f
SriBalan <105964659+sribalan98@users.noreply.github.com>
2023-11-03 03:35:01 +0530

    Update 3 local & global variable.js

--- javascript/3 local & global variable.js
+++ javascript/3 local & global variable.js
@@ -1,3 +1,29 @@
 //* LOCAL SCOPE AND GLOBAL SCOPE
 //* example 1 
 //* In this example, we will declare variables in the global scope so that they can be accessed anywhere in the program.
+
+let petName = 'Rossi' // globle scope
+
+function dogName() {
+     dogFood = 'DryFood' // global scope
+    console.log(`${petName} Love this ${dogFood}`)
+}
+// dogName()
+// console.log(`A bro give this ${dogFood} to ${petName}`)
+
+//* example 2
+//* In this example, we will declare variables in the local scope and try to access them at different scopes.
+
+readyfunction()
+makefunction()
+
+let realPetName; // global scope
+function readyfunction() {
+    let realPetName = 'myle'// local scope
+    console.log(realPetName)
+}
+function makefunction() {
+    let realPetName = 'roosi'// local scope
+    console.log(realPetName)
+}
+console.log(realPetName) //global scope log
