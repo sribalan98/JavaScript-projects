81eebbd521c9c0a1eb39ccd5ac454a3445a53df6
SriBalan <105964659+sribalan98@users.noreply.github.com>
2024-01-05 01:16:55 +0530

    server-js not today to work

--- basic-serverJs/node-js-2/server.js
+++ basic-serverJs/node-js-2/server.js
@@ -1,1 +1,28 @@
-ÿþ
+const fs = require("fs").promises;
+const path = require("path");
+const filePath = path.join(__dirname, "txtFile", "hello.txt");
+
+const fileOS = async (readfile) => {
+  try {
+    await fs.writeFile(filePath, "Hello world!");
+    readFile();
+    await fs.appendFile(filePath, " sribalan");
+    readFile();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fileOS();
+
+async function readFile() {
+  try {
+    const readFile = await fs.readFile(filePath, "utf8");
+    return console.log(` readFile  : ${readFile}`);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(__dirname);
+console.log(__filename);
